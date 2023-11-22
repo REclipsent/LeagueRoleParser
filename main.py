3157aa14ef3bf85fb0091dd9e3a2e1ae01ed88f7
REclipsent <wmtinf01@infinitymec.com>
2023-11-21 21:22:05 -0600

    Inital

---
+++ main.py
@@ -0,0 +1,65 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+def ReadPage(url):
+    # Make a request to the website
+    response = requests.get(url)
+
+    # Parse the HTML content of the page
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the second table on the page
+    tables = soup.find_all('table')
+    second_table = tables[1]  # Assuming the second table, adjust the index if needed
+
+    # Initialize empty lists for data
+    data_list = []
+
+    # Loop through rows in the second table
+    for j, row in enumerate(second_table.find_all('tr')):
+        # Skips first
+        if j == 0:
+            continue
+
+        # Initialize an empty dictionary for each row
+        row_data = {}
+
+        # Loop through cells in the row
+        for i, cell in enumerate(row.find_all('td')):
+            # Check if the cell contains an image
+            img_tag = cell.find('img')
+
+            if img_tag:
+                img_alt = img_tag.get('alt', '')
+                if img_alt == "Official":
+                    img_alt = ""
+                row_data[f'column{i + 1}'] = img_alt
+            else:
+                cellText = cell.get_text(strip=True)
+                if cellText == "OP":
+                    cellText = ""
+                row_data[f'column{i + 1}'] = cellText
+
+        # Add the row data to the list
+        data_list.append(row_data)
+
+    # Create a Pandas DataFrame from the list of dictionaries
+    df = pd.DataFrame(data_list)
+
+    df = df.fillna('')
+    df.columns = ["Champion", "Top", "Jungle", "Mid", "Bot", "Support", "Undefined"]
+    roleList = ["Top", "Jungle", "Mid", "Bot", "Support", "Undefined"]
+    for role in roleList:
+        df[role] = df[role] == 'Yes'
+    df = df.iloc[:, :-1]
+    return df
+
+def main():
+    ParseURL = 'https://leagueoflegends.fandom.com/wiki/List_of_champions_by_draft_position'
+    champFrame = ReadPage(ParseURL)
+    print(champFrame)
+    champFrame.to_csv('Champs.csv', index=False)
+
+if __name__ == "__main__":
+    main()
